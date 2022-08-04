ab483f94a69acc22a17c7c9e7a7f6838d492b366
allen dean <allen7242@gmail.com>
2022-08-04 12:47:30 -0500

    fix comment.js

--- comment.js
+++ comment.js
@@ -9,16 +9,17 @@ export default class Comment extends HTMLElement {
      }
 
      attributeChangedCallback(property, oldValue, newValue) {
+      if (oldValue === newValue) return;
 
-      if (property === "name"){
-        this.commentPlaceholder.textContent = newValue;
+      if (property === "name") {
+        this.namePlaceholder.textContent = newValue;
       }
   
-      if (property === "email"){
-        this.commentPlaceholder.textContent = newValue;
+      if (property === "email") {
+        this.emailPlaceholder.textContent = newValue;
       }
   
-      if (property === "comment"){
+      if (property === "comment") {
         this.commentPlaceholder.textContent = newValue;
       }
 
@@ -29,7 +30,7 @@ export default class Comment extends HTMLElement {
         const shadow = this.attachShadow({ mode: "open" });
         // Get the template we made in our HTML and clone it so we can use it in our component
         const template = document
-          .getElementById("#template")
+          .getElementById("template")
           .content.cloneNode(true);
     
         // Add the template to our shadow root
@@ -37,7 +38,7 @@ export default class Comment extends HTMLElement {
 
         this.namePlaceholder = this.shadowRoot.querySelector("#usersname");
         this.emailPlaceholder = this.shadowRoot.querySelector("#users-email");
-        this.commentPlaceholder = this.shadowRoot.querySelector("#uesrs-comment");
+        this.commentPlaceholder = this.shadowRoot.querySelector("#users-comment");
 
         const name = this.getAttribute("name");
         if (name) {
@@ -46,19 +47,19 @@ export default class Comment extends HTMLElement {
         
         const email = this.getAttribute("email");
         if (email) {
-          this.namePlaceholder.textContent = name;
+          this.emailPlaceholder.textContent = email;
         }
 
         const comment = this.getAttribute("comment");
         if (comment) {
-          this.namePlaceholder.textContent = comment;
+          this.commentPlaceholder.textContent = comment;
         }
 
       }
 
 }
 
-customElements.define("comment-component", Comment);
+customElements.define("comment-com", Comment);
 
 export const load = () => {
 
@@ -67,7 +68,7 @@ export const load = () => {
   const commentValue = document.getElementById("comment").value;
 
 
-  const component = document.createElement("comment-component");
+  const component = document.createElement("comment-com");
 
   component.setAttribute("name", nameValue);
   component.setAttribute("email", emailValue);
@@ -79,189 +80,4 @@ export const load = () => {
 };
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-  // component.setAttribute("name", nameValue);
-
-
-  // document.querySelector("form").addEventListener("submit", () => {
-    
-  
-  // });
-
-
-
-
-
-
-
-
-//      // Look for changes in the "rate" attribute in HTML
-//     static get observedAttributes() {
-//       return ["rate"];
-//     }
-  
-//     // Do something when an attribute has changed
-//     attributeChangedCallback(property, oldValue, newValue) {
-//       // If nothing's changed, stop execution
-//       if (oldValue === newValue) return;
-  
-//       if (property === "rate") {
-//           if (this.rate) {
-//           this.rate.textContent = newValue;
-//         }
-//       }
-//     }
-  
-//     // connectedCallback() {
-//     //   // Create a new "open" shadow root so we can manipulate it
-//     //   const shadow = this.attachShadow({ mode: "open" });
-//     //   // Get the template we made in our HTML and clone it so we can use it in our component
-//     //   const template = document
-//     //     .getElementById("user-info")
-//     //     .content.cloneNode(true);
-  
-//       // Add the template to our shadow root
-//       shadow.append(template);
-      
-//        this.shadowRoot.querySelector(
-//         'img '
-//       ).src = this.getAttribute('image');
-  
-//       // Save the element we want to use for "name" so we can set it later
-//       this.shadowRoot.querySelector("span").textContent = this.getAttribute("description");
-//         }
-//   }
-  
-//   customElements.define("user-info", UserInfo);
-  
-//   document.addEventListener("DOMContentLoaded", () => {
-//     const user = document.querySelector("#user");
-//     const form = document.querySelector("#form");
-//     const imageInput = document.querySelector("#image");
-//     const descriptionInput = document.querySelector("#description");
-  
-//     form.addEventListener("submit", (e) => {
-//       e.preventDefault();
-      
-//       const imageValue = imageInput.value;
-//       const descriptionValue = descriptionInput.value;
-      
-//       if(!imageValue || !descriptionValue) {
-//         console.log("Please validate your input?")
-//       } else {
-//       const imageComponent = document.createElement('user-info');
-      
-//       imageComponent.setAttribute("image", imageValue);
-//       imageComponent.setAttribute("description", descriptionValue);
-      
-//       user.appendChild(imageComponent);
-//       imageInput.value = "";
-//       descriptionInput.value= "";
-//       }
-//     });
-    
-    
-//   });
-
-  
-// customElements.define("custom-comment", Comment);
-
-
-
-
-
-
-
-
-
-
-
-
-//   // Define a custom element
-// class UserInfo extends HTMLElement {
-//     constructor() {
-//       super();
-//       this.rate;
-//     }
-    
-//     // Look for changes in the "rate" attribute in HTML
-//     static get observedAttributes() {
-//       return ["rate"];
-//     }
-  
-//     // Do something when an attribute has changed
-//     attributeChangedCallback(property, oldValue, newValue) {
-//       // If nothing's changed, stop execution
-//       if (oldValue === newValue) return;
-  
-//       if (property === "rate") {
-//           if (this.rate) {
-//           this.rate.textContent = newValue;
-//         }
-//       }
-//     }
-  
-//     connectedCallback() {
-//       // Create a new "open" shadow root so we can manipulate it
-//       const shadow = this.attachShadow({ mode: "open" });
-//       // Get the template we made in our HTML and clone it so we can use it in our component
-//       const template = document
-//         .getElementById("user-info")
-//         .content.cloneNode(true);
-  
-//       // Add the template to our shadow root
-//       shadow.append(template);
-      
-//        this.shadowRoot.querySelector(
-//         'img '
-//       ).src = this.getAttribute('image');
-  
-//       // Save the element we want to use for "name" so we can set it later
-//       this.shadowRoot.querySelector("span").textContent = this.getAttribute("description");
-//         }
-//   }
-  
-//   customElements.define("user-info", UserInfo);
-  
-//   document.addEventListener("DOMContentLoaded", () => {
-//     const user = document.querySelector("#user");
-//     const form = document.querySelector("#form");
-//     const imageInput = document.querySelector("#image");
-//     const descriptionInput = document.querySelector("#description");
-  
-//     form.addEventListener("submit", (e) => {
-//       e.preventDefault();
-      
-//       const imageValue = imageInput.value;
-//       const descriptionValue = descriptionInput.value;
-      
-//       if(!imageValue || !descriptionValue) {
-//         console.log("Please validate your input?")
-//       } else {
-//       const imageComponent = document.createElement('user-info');
-      
-//       imageComponent.setAttribute("image", imageValue);
-//       imageComponent.setAttribute("description", descriptionValue);
-      
-//       user.appendChild(imageComponent);
-//       imageInput.value = "";
-//       descriptionInput.value= "";
-//       }
-//     });
-    
-    
-//   });
+// setupCounter(document.querySelector("#counter"));
